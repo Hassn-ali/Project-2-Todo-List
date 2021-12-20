7dfaf83ce2f0aa33351e7a7c5d235b58b14f9e2c
Hassn-ali <xx50xx25@gmail.com>
2021-12-20 18:11:18 +0300

    finish put and delete

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,9 @@ app.get('/',(req,res)=>{
     res.json('GET / is Working')
     
 })
-
+// جلب البيانات المخزنة في قاعدة البيانات
 app.get("/tasks", (req, res) => {
+  // .find 
     Todo.find({}, (err, data) => {
       if (err) {
         console.log("ERROR: ", err);
@@ -21,7 +22,7 @@ app.get("/tasks", (req, res) => {
 });
 
 
-
+// انشاء عنصر اضافي 
   app.post("/tasks", (req, res) => {
       
     console.log('25:',req.body);
@@ -33,7 +34,42 @@ app.get("/tasks", (req, res) => {
       }
    });
   });
+  app.delete("/tasks/:id", (req, res) => {
+    console.log('25:',req.params.id);
+
+   Todo.deleteOne({_id:req.params.id}, (err, deleteObj) =>{
+         if (err) { 
+   console.log("ERROR: ", err)   ;
+          } else {
+            deleteObj.deletedCount  ===1
+            ? res.json("Delete one todo successfully")
+            : res.status(404).json("This todo is not found");
+                                   
+      }
+   });
+  });
+
+  app.put("/tasks/:id", (req, res) => {
+    // console.log('35:',req.params.id);
+    //نحدد العنصر اللي نبي نغير عليه(نحدد العنصر وذالك باستخدامid:نحط موقع ويجيب id)----(فيtitle نحدد العنصر :نكتب كود ليستقبل البيانات ويجيبها ويخزنها )
+   Todo.updateOne({_id:req.params.id},
+    { title: req.body.newTitle },
+     (err, updateObj) =>{
+         if (err) { 
+   console.log("ERROR: ", err)   ;
+          } else {
+            console.log(updateObj)
+            updateObj.modifiedCount  ===1
+            ? res.json("update one todo successfully")
+            : res.status(404).json("This todo is not found");
+                                   
+      }
+   });
+  });
    
+
+
+
 app.listen(5000,()=>{
     console.log('SERVER IS WORKING')
 })
