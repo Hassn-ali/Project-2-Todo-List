d93cb80670ab5d82959b85afc392bf1023238b3c
Hassn-ali <xx50xx25@gmail.com>
2021-12-21 21:24:03 +0300

    finish useEffect and map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import './App.css'
+import axios from 'axios'
+import Todo from './components/Todo'
 export default function App() {
  
- const getData=()=>{
-
- }
+  const [tasks,setTasks] = useState([])
  
+ useEffect(() => {
+  getData()
+   
+ }, [])
+ 
+ 
+  const getData=()=>{
+   /*should bring data using axios
+   from backend (GET/tasks) */ 
+ axios
+ .get(`http://localhost:5000/tasks`)
+.then((response) => {
+  // console.log ('RESPONSE: ', response);
+ console.log('DATA:' , response.data);
+ setTasks(response.data)
+})
+.catch((err) => {
+ console.log('ERR: ', err);
+});
+                     
+ }
+ const mapOverTasks=tasks.map((taskObj,i)=>(
+  //  here the value return from Todo 
+   <Todo key={i} task={taskObj}/>
+ )
+//  الكود اللي تحت اذا ابي ارجع القيمة من APP
+  //{  return <p>{taskObj.title}</p>}
+ );
   return (
     <div className='App'>
-      <p></p>
+      <p>app</p>
+      {/*عندما نضغط ينادي الفنكشن ويجلب البيانات */}
       <button onClick={getData}>GET TASKS</button>
+   
+{mapOverTasks}
+
+
     </div>
   )
 }
